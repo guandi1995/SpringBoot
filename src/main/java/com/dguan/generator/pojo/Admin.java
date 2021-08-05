0f1c50493992f6607839eb8454e2fe487648f116
Di Guan <dguan@purdue.edu>
2021-08-05 09:45:49 +0800

    mbtp

---
+++ src/main/java/com/dguan/generator/pojo/Admin.java
@@ -0,0 +1,62 @@
+package com.dguan.generator.pojo;
+
+import java.io.Serializable;
+
+public class Admin implements Serializable {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", name=").append(name);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/dguan/mybatis_plus/test.java
@@ -0,0 +1,8 @@
+package com.dguan.mybatis_plus;
+
+/**
+ * @author Di Guan on 8/5/2021 9:45 AM
+ */
+
+public class test {
+}
